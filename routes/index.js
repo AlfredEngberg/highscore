05d30e2299425b7d5d8c0cba6bc3fae4b3a09304
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2024-11-20 10:58:00 +0100

    added data from database

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+const pool = require('../db')
+
 router.get('/', function (req, res) {
     res.render('index.njk', { title: 'Welcome' })
 })
 
-router.get('/highscore', function (req, res) {
-    res.render('index.njk', { title: 'Highscore' })
+router.get('/highscore', async function (req, res) {
+    try {
+        const [scores] = await pool.promise().query(
+            `SELECT * FROM score AS scores`
+        );
+        console.log(scores)
+        return res.render('highscore.njk', {
+            title: 'highscore',
+            scores: scores
+        })
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+    // res.render('index.njk', { title: 'Highscore' })
 })
 
 module.exports = router
