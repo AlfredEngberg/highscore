a83686ba505cf107f4470e5c3644947e56e9157c
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2024-11-25 14:52:32 +0100

    changed to dropdown in post highscore

--- routes/index.js
+++ routes/index.js
@@ -42,21 +42,23 @@ LIMIT 10`
 
 // Get new highscorwe page
 router.get('/newHighscore', async function (req, res) {
-    res.render('newHighscore.njk')
+    const [games] = await pool.promise().query('SELECT * FROM game AS game');
+  /*   console.log(games) */
+    res.render('newHighscore.njk', {games})
 })
 
 // post high score route
 router.post('/newHighscore', async function (req, res) {
     const username = req.body.username
     const score = req.body.score
-    const game = req.body.game
+    const game = req.body.gameType
 
     try {
-        const [result] = await pool.promise().query('INSERT INTO score (score.score, score.user_id, score.game_id) VALUES (?, ?, ?);', [score, 1, 1])
+        const [result] = await pool.promise().query('INSERT INTO score (score, score.user_id, score.game_id) VALUES (?, ?, ?);', [score, username, game])
         console.log(result)
         return res.redirect('/')
     } catch (error) {
-        console.log('DET BLEV FEL')
+        console.log('DET BLEV FEL I POST HIGHSCORE')
         console.log(error)
         return res.json(error)
     }
